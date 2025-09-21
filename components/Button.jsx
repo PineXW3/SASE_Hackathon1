c9c9f1963d4f41f614890eabc4fee54351b8b5c2
Xiangtao Wu <appleafter321@gmail.com>
2025-09-21 14:05:16 -0400

    Delete components directory

--- components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button(props) {
-    return (
-        <button className={props.className}>
-            {props.name}
-        </button>
-    );
-}
-
-export default Button;
--- components/HackerInfo.jsx
+++
@@ -1,9 +0,0 @@
- function HackerInfo() {
-    return (
-       <div className="hacker-card">
-         <h2>Hacker Name</h2>
-         <p>Project Idea</p>
-       </div>
-    );
- }
- export default HackerInfo
